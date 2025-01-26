1c2f3d3cbdd88850f793064ca4323d6b99d14d45
Skifskii <petya-koreshkov@yandex.ru>
2025-01-26 18:46:48 +0300

    пишем EventProcessor

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -1,13 +1,116 @@
 package telegram
 
-import "main/clients/telegram"
+import (
+	"errors"
+	"main/clients/telegram"
+	"main/events"
+	"main/lib/e"
+	"main/storage"
+)
 
 type EventProcessor struct {
-	tg     *telegram.Client
-	offset int
-	// storage
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
 }
 
-func New(client *telegram.Client) {
+type Meta struct {
+	ChatID   int
+	Username string
+}
 
+var (
+	ErrUnknownEventType = errors.New("unknown event type")
+	ErrUnknownMetaType  = errors.New("unknown meta type")
+)
+
+func New(client *telegram.Client, storage storage.Storage) *EventProcessor {
+	return &EventProcessor{
+		tg:      client,
+		storage: storage,
+	}
+}
+
+func (ep *EventProcessor) Fetch(limit int) ([]events.Event, error) {
+	updates, err := ep.tg.Updates(ep.offset, limit)
+	if err != nil {
+		return nil, e.Wrap("can't get events", err)
+	}
+
+	if len(updates) == 0 {
+		return nil, nil
+	}
+
+	res := make([]events.Event, 0, len(updates))
+
+	for _, u := range updates {
+		res = append(res, event(u))
+	}
+
+	ep.offset = updates[len(updates)-1].ID + 1
+
+	return res, nil
+}
+
+func (ep *EventProcessor) Process(event events.Event) error {
+	switch event.Type {
+	case events.Message:
+		return ep.processMessage(event)
+	default:
+		return e.Wrap("can't proces message", ErrUnknownEventType)
+	}
+}
+
+func (ep *EventProcessor) processMessage(event events.Event) error {
+	meta, err := meta(event)
+	if err != nil {
+		return e.Wrap("can't process message", err)
+	}
+
+	if err := ep.doCmd(event.Text, meta.ChatID, meta.Username); err != nil {
+		return e.Wrap("can't process message", err)
+	}
+
+	return nil
+}
+
+func meta(event events.Event) (Meta, error) {
+	res, ok := event.Meta.(Meta)
+	if !ok {
+		return Meta{}, e.Wrap("can't get meta", ErrUnknownMetaType)
+	}
+
+	return res, nil
+}
+
+func event(upd telegram.Update) events.Event {
+	updType := fetchType(upd)
+
+	res := events.Event{
+		Type: updType,
+		Text: fetchText(upd),
+	}
+
+	if updType == events.Message {
+		res.Meta = Meta{
+			ChatID:   upd.Message.Chat.ID,
+			Username: upd.Message.From.Username,
+		}
+	}
+
+	return res
+}
+
+func fetchText(upd telegram.Update) string {
+	if upd.Message == nil {
+		return ""
+	}
+	return upd.Message.Text
+}
+
+func fetchType(upd telegram.Update) events.Type {
+	if upd.Message == nil {
+		return events.Unknown
+	}
+	return events.Message
 }
--- storage/files/files.go
+++ storage/files/files.go
@@ -17,8 +17,6 @@ type Storage struct {
 
 const defaultPerm = 0774
 
-var ErrNoSavedPages = errors.New("no saved pages")
-
 func New(basePath string) Storage {
 	return Storage{baseBath: basePath}
 }
@@ -62,7 +60,7 @@ func (s Storage) PickRandom(userName string) (page *storage.Page, err error) {
 	}
 
 	if len(files) == 0 {
-		return nil, ErrNoSavedPages
+		return nil, storage.ErrNoSavedPages
 	}
 
 	n := rand.Intn(len(files))
